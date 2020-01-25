46298c0279585dae63071ef717ca09d3bcf5d9f4
Aykuli <aykuli@ya.ru>
2020-01-25 17:17:19 +0700

    feat: add Readme file, fix styles

---
+++ README.md
@@ -0,0 +1,5 @@
+# Chuck Norris Joke Generator
+
+Creator: [Aynur Shauerman](https://github.com/aykuli)
+
+![screenshot](./screenshot.jpg)
--- jokes.js
+++ jokes.js
@@ -1,6 +1,6 @@
 export const jokes ={
     getOne: function () {
-        return new Promise((resolve, reject) => {
+        return new Promise((resolve) => {
             fetch('http://api.icndb.com/jokes/random')
             .then(res => res.json())
             .then(data => {
